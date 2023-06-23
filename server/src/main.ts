231fa6ec75ee4f6a30fe3d0cb64813d8c192a681
jiminKang <kang@jiminKangs-MacBook-Pro.local>
2023-06-23 18:44:36 +0900

    웹팩을 실행하기 위한 명령어 셋팅

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,7 +7,7 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useStaticAssets(join(__dirname,'..','../public'))
+  app.useStaticAssets(join(__dirname,'../public'))
   
   await app.listen(3000);
 }
