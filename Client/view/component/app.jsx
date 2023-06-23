39a22957244036cded3d4a9fc6515d1dcd80d63a
jiminKang <kang@jiminKangs-MacBook-Pro.local>
2023-06-23 18:22:57 +0900

    코드 작성

---
+++ Client/view/component/app.jsx
@@ -0,0 +1,9 @@
+import React, {Component, useState} from 'react';
+
+function App(){
+    return (
+        <div>hello 포람페!</div>
+    )
+}
+
+export default App
---
+++ Client/view/component/index.jsx
@@ -0,0 +1,6 @@
+import react from 'react';
+import {createRoot} from 'react-dom/client'
+import App from ''
+
+const root = createRoot(document.getElementById('root'))
+root.render(<App></App>)
