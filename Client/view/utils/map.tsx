bc0d425be895c41bbaba95a56d479a59b0a6d907
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-26 17:01:15 +0900

    마커 불러오기 실패. 다시 한번 진행해야 할 것 같다

--- Client/view/utils/map.tsx
+++ Client/view/utils/map.tsx
@@ -54,10 +54,21 @@ export default function Map() {
   }
   useEffect(() => {
     const container = document.getElementsByClassName("map");
-    const options = {
-      center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+    const script = document.createElement("script");
+    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=584005aa7fee37a3ef459f49ebfc7e70&autoload=false`;
+    document.head.appendChild(script);
+    script.onload = () => {
+      window.kakao.maps.load(() => {
+        const options = {
+          center: new window.kakao.maps.LatLng(36.3016838, 127.3789012),
+          level: 3,
+        };
+        const map = new window.kakao.maps.Map(container, options); // 지도 생성
+
+        // // 지도에 교통정보를 표시하도록 지도타입을 추가합니다
+        // map.addOverlayMapTypeId(window.kakao.maps.MapTypeId.TRAFFIC);
+      });
     };
-    const map = new window.kakao.maps.Map(container, options);
   }, []);
 
   return (
