ba59df7f323ccacb20380246f57d0cf3c670924f
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-26 11:15:50 +0900

    카카오 api를 사용하여 지도 불러오기 시도 중

--- Client/view/utils/map.tsx
+++ Client/view/utils/map.tsx
@@ -3,11 +3,22 @@ import React, { useEffect } from "react";
 
 // 리액트 컴포넌트
 
+// @ts-ignore
+const { kakao } = window;
+
 export default function Map() {
+  useEffect(() => {
+    const container = document.getElementsByClassName("map");
+    const options = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+    };
+    const map = new kakao.maps.Map(container, options);
+  }, []);
+
   return (
     <>
       <div
-        className="kakaoMap"
+        className="map"
         style={{
           width: "500px",
           height: "500px",
