4b6c377cc2133d384b5a4d60036ee08731683a99
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-26 16:54:45 +0900

    현재 위치를 찾고 마커를 찍는 로직 제작 중

--- Client/view/utils/map.tsx
+++ Client/view/utils/map.tsx
@@ -7,6 +7,48 @@ import React, { useEffect } from "react";
 const { kakao } = window;
 
 export default function Map() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      let lat = position.coords.latitude;
+      let lng = position.coords.longitude;
+
+      let locPosition = new kakao.maps.LatLng(lat, lng),
+        message = `<div>현재 위치</div>`;
+
+      displayMarker(locPosition, message);
+    });
+  } else {
+    // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+
+    var locPosition = new kakao.maps.LatLng(33.450701, 126.570667),
+      message = "geolocation을 사용할수 없어요..";
+
+    displayMarker(locPosition, message);
+  }
+
+  // 지도에 마커와 인포윈도우를 표시하는 함수입니다
+  function displayMarker(locPosition, message) {
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+      map: map,
+      position: locPosition,
+    });
+
+    var iwContent = message, // 인포윈도우에 표시할 내용
+      iwRemoveable = true;
+
+    // 인포윈도우를 생성합니다
+    var infowindow = new kakao.maps.InfoWindow({
+      content: iwContent,
+      removable: iwRemoveable,
+    });
+
+    // 인포윈도우를 마커위에 표시합니다
+    infowindow.open(window.kakao.map, marker);
+
+    // 지도 중심좌표를 접속위치로 변경합니다
+    window.kakao.map.setCenter(locPosition);
+  }
   useEffect(() => {
     const container = document.getElementsByClassName("map");
     const options = {
