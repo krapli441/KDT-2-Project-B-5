7003f6007bd2e8f1be57a46df7ef3260559e9d6b
leejaekwon <nowkwon97@gmail.com>
2023-06-26 12:39:43 +0900

    UPDATE 사용자 현재 위치 마커 찍기

--- Client/view/component/kakaoMap.tsx
+++ Client/view/component/kakaoMap.tsx
@@ -9,7 +9,7 @@ declare global {
 const MapContainer = () => {
   useEffect(() => {
     const container = document.getElementById('map');
-    
+
     const script = document.createElement('script');
     script.async = true;
     script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=f3f69e507f70b20f6cdaa643fb68b19b&autoload=false`;
@@ -18,11 +18,34 @@ const MapContainer = () => {
     script.onload = () => {
       window.kakao.maps.load(() => {
         const options = {
-          center: new window.kakao.maps.LatLng(36.3494652, 127.3775608),
+          center: new window.kakao.maps.LatLng(33.450701, 126.570667),
           level: 3
         };
-      
-        new window.kakao.maps.Map(container, options);
+
+        const map = new window.kakao.maps.Map(container, options);
+
+        // 사용자의 현재 위치 가져오기
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+
+            // 현재 위치 마커 생성
+            const markerPosition = new window.kakao.maps.LatLng(latitude, longitude);
+            const marker = new window.kakao.maps.Marker({
+              position: markerPosition
+            });
+
+            // 마커를 지도에 추가
+            marker.setMap(map);
+
+            // 지도 중심 위치 변경
+            map.setCenter(markerPosition);
+          },
+          (error) => {
+            console.log('Error getting geolocation:', error);
+          }
+        );
       });
     };
   }, []);
