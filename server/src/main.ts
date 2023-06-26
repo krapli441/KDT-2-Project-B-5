cbac32f45a34a7805001fd962924640ee37e6376
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-26 17:11:51 +0900

    SSL 인증서 추가

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,12 +4,21 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { ServeStaticModule } from '@nestjs/serve-static';
 
-
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useStaticAssets(join(__dirname,'../public'))
-  
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    // https 옵션을 설정한다.
+    // fs.readFileSync를 사용하여 로컬에 있는 localhost.key 파일과
+    // localhost.crt 파일의 내용을 읽어온다.
+    // 이를 https 서버 구성에 사용한다.
+    httpsOptions: {
+      key: fs.readFileSync('localhost.key'),
+      cert: fs.readFileSync('localhost.crt'),
+    },
+  });
+  app.useStaticAssets(join(__dirname, '../public'));
+
   await app.listen(3001);
 }
 bootstrap();
