e3fa3a6a25831d60138f32f36ab9b4f2ab686788
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-26 11:05:03 +0900

    Clinet 폴더에 개발 의존 환경으로 typescript, @types/node, @types/react, ts-loader 설치

---
+++ Client/view/utils/map.tsx
@@ -0,0 +1,18 @@
+// 리액트 라이브러리
+import React, { useEffect } from "react";
+
+// 리액트 컴포넌트
+
+export default function Map() {
+  return (
+    <>
+      <div
+        className="kakaoMap"
+        style={{
+          width: "500px",
+          height: "500px",
+        }}
+      ></div>
+    </>
+  );
+}
