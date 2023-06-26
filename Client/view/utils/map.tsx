1ed80edc23f0c4078e76bb81d43f3db3b522ac2e
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-26 11:42:33 +0900

    타입스크립트 환경에서 카카오 지도 API 가져오기

--- Client/view/utils/map.tsx
+++ Client/view/utils/map.tsx
@@ -3,18 +3,14 @@ import React, { useEffect } from "react";
 
 // 리액트 컴포넌트
 
-// @ts-ignore
-const { kakao } = window;
+// 
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
 
 export default function Map() {
-  useEffect(() => {
-    const container = document.getElementsByClassName("map");
-    const options = {
-      center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-    };
-    const map = new kakao.maps.Map(container, options);
-  }, []);
-
   return (
     <>
       <div
