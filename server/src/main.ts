ebc56d03e465170b2a68f40ec6a90fea4a399eab
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-26 15:43:44 +0900

    SSL 인증서를 이용하여 http를 https로 바꾸었음.

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,13 +3,18 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { ServeStaticModule } from '@nestjs/serve-static';
-
-
+import * as fs from 'fs';
+import * as https from 'https';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useStaticAssets(join(__dirname,'../public'))
-  
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    httpsOptions: {
+      key: fs.readFileSync('localhost.key'),
+      cert: fs.readFileSync('localhost.crt'),
+    },
+  });
+  app.useStaticAssets(join(__dirname, '../public'));
+
   await app.listen(3001);
 }
 bootstrap();
