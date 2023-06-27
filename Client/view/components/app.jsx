3215b4524fbace50127418633253d6cfa3ff3952
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-27 14:36:29 +0900

    네이버 지도 API를 불러오기 위해 기초 세팅

--- Client/view/components/app.jsx
+++ Client/view/components/app.jsx
@@ -2,7 +2,7 @@
 import React, { Component, useState } from "react";
 
 // 리액트 컴포넌트
-import Map from "../utils/map";
+import Map from "../utils/kakaoMap";
 
 function App() {
   return (
---
+++ Client/view/utils/naverMap.tsx
@@ -0,0 +1,7 @@
+// 리액트 라이브러리
+import React from "react";
+
+// 리액트 컴포넌트
+import LocationComponent from "./getUserLocation";
+
+
