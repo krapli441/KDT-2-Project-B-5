7e7f7644726ad152010053bfc1991e879ba1de05
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-27 12:50:55 +0900

    setTimeOut도 잘 먹히고 있다. 어떤 점이 문제인지 파악 필요.

--- Client/view/utils/map.tsx
+++ Client/view/utils/map.tsx
@@ -34,27 +34,29 @@ const MapContainer = () => {
     }
 
     // * 카카오 지도 API 로직
-    const container = document.getElementById("map");
-    const script = document.createElement("script");
-    script.async = true;
-    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=584005aa7fee37a3ef459f49ebfc7e70&autoload=false`;
-    document.head.appendChild(script);
-    script.onload = () => {
-      window.kakao.maps.load(() => {
-        const options = {
-          center: new window.kakao.maps.LatLng(36.3016838, 127.3789012),
-          level: 3,
-        };
-        const map = new window.kakao.maps.Map(container, options); // 지도 생성
+    setTimeout(() => {
+      const container = document.getElementById("map");
+      const script = document.createElement("script");
+      script.async = true;
+      script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=584005aa7fee37a3ef459f49ebfc7e70&autoload=false`;
+      document.head.appendChild(script);
+      script.onload = () => {
+        window.kakao.maps.load(() => {
+          const options = {
+            center: new window.kakao.maps.LatLng(36.3016838, 127.3789012),
+            level: 3,
+          };
+          const map = new window.kakao.maps.Map(container, options); // 지도 생성
 
-        // 지도에 교통정보를 표시하도록 지도타입을 추가합니다
-        map.addOverlayMapTypeId(window.kakao.maps.MapTypeId.TRAFFIC);
-        let marker = new window.kakao.maps.Marker({
-          position: new window.kakao.maps.LatLng(36.3016838, 127.3789012), // 마커의 좌표
-          map: map, // 마커를 표시할 지도 객체
+          // 지도에 교통정보를 표시하도록 지도타입을 추가합니다
+          map.addOverlayMapTypeId(window.kakao.maps.MapTypeId.TRAFFIC);
+          let marker = new window.kakao.maps.Marker({
+            position: new window.kakao.maps.LatLng(36.3016838, 127.3789012), // 마커의 좌표
+            map: map, // 마커를 표시할 지도 객체
+          });
         });
-      });
-    };
+      };
+    }, 3000);
   }, []);
   return (
     <>
