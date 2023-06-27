e3139fe8cfecc311311e0638a58e3d992c41c2be
jiminKang <kang@jiminKangs-MacBook-Pro.local>
2023-06-27 20:39:36 +0900

    환경 변수 파일 설정 안 먹는데?

--- Client/view/component/app.jsx
+++ Client/view/component/app.jsx
@@ -1,35 +1,58 @@
-import React, {Component, useState} from 'react';
+import React, { useEffect } from 'react';
+import { useKakaoMaps } from 'react-kakao-maps';
 
-function App(){
-    // fetch('https://openapi.its.go.kr:9443/vdsInfo?apiKey=6c0ba5b5a21845f1b2b9090f31af33a3&getType=json').then(res=>res).then(data=>console.log(data))
-    var url = 'https://openapi.its.go.kr:9443/vdsInfo?apiKey=6c0ba5b5a21845f1b2b9090f31af33a3&getType=json'
-    // var params = "apiKey=" + encodeURIComponent("test") + "&getType=" + encodeURIComponent("xml");
-fetch(url  ,{
-  method: "GET",
-  headers: {
-    "Content-type": "text/xml;charset=UTF-8"
-  }
-})
-.then(function(response) {
-  if (response.ok) {
-    console.log("Response code: " + response.status);
-    return response.text();
-  } else {
-    throw new Error("Error: " + response.status);
-  }
-})
-.then(function(data) {
-  console.log(data);
-})
-.catch(function(error) {
-  console.log(error);
-});
+function TrafficStatus() {
+  const REACT_APP_KAKAO_MAPS_API_KEY='b3da2e1025f79e2251178086a01fa93b'
+  const { kakao, mapRef } = useKakaoMaps(process.env.REACT_APP_KAKAO_MAPS_API_KEY);
 
-    return (
-        <div>
-            <p>hello 포람페!</p>
-        </div>
-    )
+  useEffect(() => {
+    if (kakao) {
+      // 현재 위치 가져오기
+      navigator.geolocation.getCurrentPosition(function(position) {
+        const lat = position.coords.latitude; // 위도
+        const lng = position.coords.longitude; // 경도
+        const currentPosition = new kakao.maps.LatLng(lat, lng);
+
+        // 지도 생성
+        const mapOptions = {
+          center: currentPosition,
+          level: 15
+        };
+        const map = new kakao.maps.Map(mapRef.current, mapOptions);
+
+        // 현재 위치 마커 생성
+        const marker = new kakao.maps.Marker({
+          position: currentPosition,
+          map: map
+        });
+
+        // 현재 위치 기준으로 교통 상태 확인
+        const traffic = new kakao.maps.services.Traffic();
+        traffic.getTrafficInfo(currentPosition, function(result, status) {
+          if (status === kakao.maps.services.Status.OK) {
+            const congestion = result.status; // 혼잡 상태 (1: 원할, 2: 보통, 3: 혼잡, 4: 매우 혼잡)
+
+            // 교통 상태에 따라 메시지 출력
+            if (congestion >= 3) {
+              alert('교통상태가 혼잡합니다.');
+            } else {
+              alert('교통상황이 원활합니다.');
+            }
+          }
+        });
+
+        // 지도 중심 위치를 현재 위치로 설정
+        map.setCenter(currentPosition);
+      });
+    }
+  }, [kakao, mapRef]);
+
+  return (
+    <div>
+      <h1>현재 위치의 교통 상태 확인</h1>
+      <div ref={mapRef} style={{ width: '100%', height: '400px' }}></div>
+    </div>
+  );
 }
 
-export default App
+export default TrafficStatus;
