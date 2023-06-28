85e27769ecf7e65d27b9fab6e6b610a39f965c41
wjkk21 <verstand821@gmail.com>
2023-06-28 12:49:23 +0900

    app.tsx 파일 변경 중

--- Client/src/app.tsx
+++ Client/src/app.tsx
@@ -1,11 +1,39 @@
 import React, { Component, useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-function App() {
+import Intro from "../view/IntroPage/IntroPageScreen";
+import login from "../view/loginPage/loginScreen";
+import Firstlogin from "../view/firstLoginPage/";
+import  from "../view/fragments";
+import Nav from "../view/fragments/footer";
+
+export default function App() {
+
   return (
-    <div>
-      <p>hello 포람페!</p>
-    </div>
+    <>
+      <div className="container">
+          <Routes>
+            <Route path="/" element={<IntroPage />} />
+            <Route path="/home" element={<MainScreen />} />
+            <Route path="/first" element={<FirstPage />} />
+            <Route path="/login" element={<LoginScreen />} />
+            <Route path="/signup" element={<SignUpScreen />} />
+            <Route path="/welcome_1" element={<FirstLoginPage />} />
+            <Route path="/welcome_2" element={<WelcomeSecond />} />
+            <Route path="/welcome_3" element={<WelcomeThird />} />
+          </Routes>
+        {[
+          "/",
+          "/first",
+          "/signup",
+          "/login",
+          "/welcome_1",
+          "/welcome_2",
+          "/welcome_3",
+        ].includes(location.pathname) ? null : (
+          <Nav />
+        )}
+      </div>
+    </>
   );
 }
-
-export default App;
--- Client/view/fragments/footer.tsx
+++ Client/view/fragments/footer.tsx
@@ -1,4 +1,7 @@
 // 리액트 라이브러리
 import React from "react";
 
+export default function Nav() {
+  return <div></div>;
+}
 // 리액트 컴포넌트
