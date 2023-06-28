13713d62939a78f5bdac0fb16e973cd9a67565d7
krapli441 <tlfqj1208@gmail.com>
2023-06-29 00:48:20 +0900

    lottie player 설치 및 dotlottie 파일 추가

--- Client/view/pages/IntroPage/introPage.tsx
+++ Client/view/pages/IntroPage/introPage.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Box } from "@chakra-ui/react";
+import "@dotlottie/player-component";
 
 // 리액트 컴포넌트
 import IntroLogo from "./introLogo.png";
@@ -24,6 +25,11 @@ function introPage() {
         alignItems={"center"}
         backgroundColor={"white"}
       >
+        <dotlottie-player
+          src="../../components/asphalt.lottie"
+          autoplay
+          style={{ width: "347px", height: "99px" }}
+        />
         <img src={IntroLogo} width="70%" alt="로고" />
       </Box>
     </>
