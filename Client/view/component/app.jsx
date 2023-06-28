e4a4417f16861f6384a7416c24020b641da2fa52
jiminKang <kang@jiminKangs-MacBook-Pro.local>
2023-06-28 10:08:55 +0900

    지피티가 제공해준 코드 (비동기 작업 중 오류 발생)

--- Client/view/component/app.jsx
+++ Client/view/component/app.jsx
@@ -1,61 +1,56 @@
-import React, { useEffect } from 'react';
-import { useKakaoMaps } from 'react-kakao-maps';
+import React, { useEffect, useState } from 'react';
 
-function TrafficStatus() {
-  
-  const apiKey = 'b3da2e1025f79e2251178086a01fa93b'; // 본인의 Kakao Maps API 키로 대체
-
-  const { kakao, mapRef } = useKakaoMaps(apiKey);
-
+const TrafficStatus = () => {
+  const [traffic, setTraffic] = useState('');
 
   useEffect(() => {
-    if (kakao) {
-      // 현재 위치 가져오기
-      navigator.geolocation.getCurrentPosition(function(position) {
-        const lat = position.coords.latitude; // 위도
-        const lng = position.coords.longitude; // 경도
-        const currentPosition = new kakao.maps.LatLng(lat, lng);
+    const getTrafficStatus = async () => {
+      try {
+        // 현재 위치 좌표를 얻는 코드 (navigator.geolocation 사용)
+        navigator.geolocation.getCurrentPosition(async (position) => {
+          const { latitude, longitude } = position.coords;
 
-        // 지도 생성
-        const mapOptions = {
-          center: currentPosition,
-          level: 15
-        };
-        const map = new kakao.maps.Map(mapRef.current, mapOptions);
+          // 카카오 API 요청 URL
+          const url = `https://dapi.kakao.com/v2/local/geo/transcoord.json?x=${longitude}&y=${latitude}&input_coord=WGS84&output_coord=TM`;
 
-        // 현재 위치 마커 생성
-        const marker = new kakao.maps.Marker({
-          position: currentPosition,
-          map: map
-        });
+          // 카카오 API 요청
+          const response = await fetch(url, {
+            headers: {
+              Authorization: `b3da2e1025f79e2251178086a01fa93b`,
+            },
+          });
 
-        // 현재 위치 기준으로 교통 상태 확인
-        const traffic = new kakao.maps.services.Traffic();
-        traffic.getTrafficInfo(currentPosition, function(result, status) {
-          if (status === kakao.maps.services.Status.OK) {
-            const congestion = result.status; // 혼잡 상태 (1: 원할, 2: 보통, 3: 혼잡, 4: 매우 혼잡)
+          const data = await response.json();
+          const { x, y } = data.documents[0];
 
-            // 교통 상태에 따라 메시지 출력
-            if (congestion >= 3) {
-              alert('교통상태가 혼잡합니다.');
-            } else {
-              alert('교통상황이 원활합니다.');
-            }
+          // 교통량 정보를 얻는 카카오 API 요청 URL
+          const trafficUrl = `https://dapi.kakao.com/v2/local/traffic/forecast/link?x=${x}&y=${y}`;
+
+          // 교통량 정보 요청
+          const trafficResponse = await fetch(trafficUrl, {
+            headers: {
+              Authorization: `KakaoAK ${YOUR_API_KEY}`,
+            },
+          });
+
+          const trafficData = await trafficResponse.json();
+
+          // 교통량 정보를 기반으로 메시지 설정
+          if (trafficData.features.length > 0) {
+            setTraffic('원활');
+          } else {
+            setTraffic('혼잡');
           }
         });
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
 
-        // 지도 중심 위치를 현재 위치로 설정
-        map.setCenter(currentPosition);
-      });
-    }
-  }, [kakao, mapRef]);
+    getTrafficStatus();
+  }, []);
 
-  return (
-    <div>
-      <h1>현재 위치의 교통 상태 확인</h1>
-      {/* <div ref={mapRef} style={{ width: '100%', height: '400px' }}></div> */}
-    </div>
-  );
-}
+  return <div>교통 상태: {traffic}</div>;
+};
 
 export default TrafficStatus;
