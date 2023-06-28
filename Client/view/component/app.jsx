56acd6f86a8f96b416d42869156a74595fc8cf88
jiminKang <kang@jiminKangs-MacBook-Pro.local>
2023-06-28 12:39:22 +0900

    kakao.maps.LatLng is not a constructor 오류 발생

--- Client/view/component/app.jsx
+++ Client/view/component/app.jsx
@@ -1,56 +1,41 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 
-const TrafficStatus = () => {
-  const [traffic, setTraffic] = useState('');
-
+const MapComponent = () => {
   useEffect(() => {
-    const getTrafficStatus = async () => {
-      try {
-        // 현재 위치 좌표를 얻는 코드 (navigator.geolocation 사용)
-        navigator.geolocation.getCurrentPosition(async (position) => {
-          const { latitude, longitude } = position.coords;
+    const loadMap = async () => {
+      const mapScript = document.createElement('script');
+      mapScript.src = 'https://dapi.kakao.com/v2/maps/sdk.js?appkey=b3da2e1025f79e2251178086a01fa93b&libraries=services';
+      document.head.appendChild(mapScript);
 
-          // 카카오 API 요청 URL
-          const url = `https://dapi.kakao.com/v2/local/geo/transcoord.json?x=${longitude}&y=${latitude}&input_coord=WGS84&output_coord=TM`;
+      mapScript.onload = () => {
+        const mapContainer = document.getElementById('map');
+        const options = {
+          center: new kakao.maps.LatLng(36.344291, 127.384876), // 둔산동 둔산대로 좌표 설정
+          level: 3,
+        };
+        const map = new kakao.maps.Map(mapContainer, options);
 
-          // 카카오 API 요청
-          const response = await fetch(url, {
-            headers: {
-              Authorization: `b3da2e1025f79e2251178086a01fa93b`,
-            },
-          });
+        const trafficService = new kakao.maps.services.Traffic();
 
-          const data = await response.json();
-          const { x, y } = data.documents[0];
+        // 둔산동 둔산대로의 실시간 교통량 정보 가져오기
+        trafficService.getRoadTrafficData(178, (data, status) => {
+          if (status === kakao.maps.services.Status.OK) {
+            const congestion = data.congestion;
 
-          // 교통량 정보를 얻는 카카오 API 요청 URL
-          const trafficUrl = `https://dapi.kakao.com/v2/local/traffic/forecast/link?x=${x}&y=${y}`;
-
-          // 교통량 정보 요청
-          const trafficResponse = await fetch(trafficUrl, {
-            headers: {
-              Authorization: `KakaoAK ${YOUR_API_KEY}`,
-            },
-          });
-
-          const trafficData = await trafficResponse.json();
-
-          // 교통량 정보를 기반으로 메시지 설정
-          if (trafficData.features.length > 0) {
-            setTraffic('원활');
-          } else {
-            setTraffic('혼잡');
+            if (congestion === '3' || congestion === '4') {
+              console.log('혼잡');
+            } else {
+              console.log('원할');
+            }
           }
         });
-      } catch (error) {
-        console.error('Error:', error);
-      }
+      };
     };
 
-    getTrafficStatus();
+    loadMap();
   }, []);
 
-  return <div>교통 상태: {traffic}</div>;
+  return <div id="map" style={{ width: '500px', height: '400px' }}></div>;
 };
 
-export default TrafficStatus;
+export default MapComponent;
