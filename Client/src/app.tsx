a7693feefe078fb8704a72d9246f5c9d27014caf
wjkk21 <verstand821@gmail.com>
2023-06-28 14:54:34 +0900

    오류로 인해 일단 코드 삭제하였습니다

--- Client/src/app.tsx
+++ Client/src/app.tsx
@@ -1,44 +1,15 @@
 import React, { Component, useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
+/* 
 import Intro from "../view/IntroPage/IntroPageScreen";
 import login from "../view/loginPage/loginScreen";
 import  from "../view/fragments";
-import Nav from "../view/fragments/footer";
+import Nav from "../view/fragments/footer"; */
 
 export default function App() {
-
   return (
-<<<<<<< HEAD
-    <>
-      <div className="container">
-          <Routes>
-            <Route path="/" element={<Intro />} />
-{/*             <Route path="/home" element={<MainScreen />} />
-            <Route path="/Intro" element={<IntroPageScreen />} />
-            <Route path="/login" element={<LoginScreen />} />
-            <Route path="/signup" element={<SignUpScreen />} />
-            <Route path="/welcome_1" element={<FirstLoginPage />} />
-            <Route path="/welcome_2" element={<WelcomeSecond />} />
-            <Route path="/welcome_3" element={<WelcomeThird />} /> */}
-          </Routes>
-{/*         {[
-          "/",
-          "/first",
-          "/signup",
-          "/login",
-          "/welcome_1",
-          "/welcome_2",
-          "/welcome_3",
-        ].includes(location.pathname) ? null : (
-          <Nav />
-        )} */}
-      </div>
-    </>
-=======
     <div>
       <p>웹팩 개발서버 테스트</p>
     </div>
->>>>>>> origin/subMain
   );
 }
