84b61cdc6e26f5da82cc9d86606d5b63bdc24788
jiminKang <kang@jiminKangs-MacBook-Pro.local>
2023-06-29 14:34:42 +0900

    Tmap API로 지도 띄우기 시도 1 실패

--- Client/view/component/maps.jsx
+++ Client/view/component/maps.jsx
@@ -1,13 +1,17 @@
-import React, { useEffect } from 'react';
-import Tmapv2 from 'tmapv2'; // Tmapv2 라이브러리를 import 해주세요.
+import React from 'react';
+import Tmap from 'react-tmap';
 
-function MapComponent() {
-  useEffect(() => {
-    fetch("https://code.jquery.com/jquery-3.2.1.min.js").then(res=>res).then(data=>console.log('data1',data))
-    fetch("https://apis.openapi.sk.com/tmap/jsv2?version=1&appKey=FB8ThRVZHU4x1zUhC432j3DKfPOdkpmrajEOCYZe").then(res=>res).then(data=>console.log('data2',data))
-  }, []);
-
-  return <div id="map_div"></div>;
+const MapComponent = () => {
+  return (
+    <div style={{ width: '100%', height: '400px' }}>
+      <Tmap
+        id="map"
+        appKey="FB8ThRVZHU4x1zUhC432j3DKfPOdkpmrajEOCYZe"
+        zoom={15}
+        center={{ lat: 37.5665, lng: 126.9780 }}
+      />
+    </div>
+  );
 }
 
 export default MapComponent;
