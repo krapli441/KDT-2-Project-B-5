c01598d2149e784f1c18741c3cb1a7401ec48849
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-29 19:38:38 +0900

    교통량을 표시하기 위해 시도 중

--- Client/utils/skTmapRemake.tsx
+++ Client/utils/skTmapRemake.tsx
@@ -3,22 +3,20 @@ import React, { useState, useEffect } from "react";
 
 // 리액트 컴포넌트
 import LocationComponent from "./getUserLocation";
+import SampleData from "./getTrafficSampleData";
 
 declare global {
   interface Window {
-    Tmapv3: any;
+    Tmapv2: any;
   }
 }
 
-let appKey = "4AfhmXH1W616IshxuKXD27orRe3ufLzD4EHChwyV";
-let centerLon = 127.3789012;
-let centerLat = 36.3016838;
-
-let requestURI = `https://apis.openapi.sk.com/tmap/traffic?version=1&format=json&reqCoordType=WGS84GEO&resCoordType=EPSG3857&zoomLevel=1&trafficType=AROUND&radius=3&centorLon=${centerLon}&centerLat=${centerLat}&appKey=${appKey}`;
+let requestURI = `https://apis.openapi.sk.com/tmap/traffic?version=${SampleData.version}&minLat=${SampleData.minLat}&minLon=${SampleData.minLon}&maxLat=${SampleData.maxLat}&maxLon=${SampleData.maxLon}&centerLat=${SampleData.centerLat}&centerLon=${SampleData.centerLon}&reqCoordType=${SampleData.reqCoordType}&resCoordType=${SampleData.resCoordType}&trafficType=${SampleData.trafficType}&zoomLevel=${SampleData.zoomLevel}&callback=${SampleData.callback}&appKey=${SampleData.appKey}`;
 
 const MapContainer: React.FC = () => {
   const [userLocation, setUserLocation] =
     useState<GeolocationCoordinates | null>(null);
+  const [polyLineArr, setPolyLineArr] = useState<any[]>([]);
 
   // * 사용자 위치 정보를 가져오는 useEffect
 
@@ -29,7 +27,7 @@ const MapContainer: React.FC = () => {
           setUserLocation(position.coords);
         },
         (error) => {
-          console.log("사용자 위치 정보를 가져오는데 실패했습니다.");
+          console.log(error);
         },
         {
           enableHighAccuracy: true,
@@ -41,22 +39,22 @@ const MapContainer: React.FC = () => {
   }, []);
 
   // 교통 정보 데이터를 받아오는 로직
-  fetch(requestURI, {
-    method: "GET",
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("교통 정보 데이터를 가져오는 중 오류가 발생했습니다.");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      // 데이터 처리
-      console.log(data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  // fetch(requestURI, {
+  //   method: "GET",
+  // })
+  //   .then((response) => {
+  //     if (!response.ok) {
+  //       throw new Error();
+  //     }
+  //     return response.json();
+  //   })
+  //   .then((data) => {
+  //     // 데이터 처리
+  //     console.log(data);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
 
   // * SK open API를 사용하여 맵을 생성하는 useEffect 훅
   // * 윗단 useEffect로 사용자 정보를 가져올 경우 실행된다
@@ -64,8 +62,8 @@ const MapContainer: React.FC = () => {
   useEffect(() => {
     if (userLocation) {
       function generateMap() {
-        let map = new window.Tmapv3.Map("TmapApp", {
-          center: new window.Tmapv3.LatLng(
+        let map = new window.Tmapv2.Map("TmapApp", {
+          center: new window.Tmapv2.LatLng(
             userLocation?.latitude,
             userLocation?.longitude
           ),
@@ -73,8 +71,81 @@ const MapContainer: React.FC = () => {
           height: "100%",
           zoom: 15,
         });
+        return map;
       }
-      generateMap();
+      const map = generateMap();
+      const requestURI = `https://apis.openapi.sk.com/tmap/traffic?version=${SampleData.version}&minLat=${SampleData.minLat}&minLon=${SampleData.minLon}&maxLat=${SampleData.maxLat}&maxLon=${SampleData.maxLon}&centerLat=${SampleData.centerLat}&centerLon=${SampleData.centerLon}&reqCoordType=${SampleData.reqCoordType}&resCoordType=${SampleData.resCoordType}&trafficType=${SampleData.trafficType}&zoomLevel=${SampleData.zoomLevel}&callback=${SampleData.callback}&appKey=${SampleData.appKey}`;
+
+      fetch(requestURI)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("요청이 실패하였습니다.");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          const resultData = data.features;
+
+          if (polyLineArr.length > 0) {
+            polyLineArr.forEach((polyline) => {
+              polyline.setMap(null);
+            });
+          }
+
+          const newPolyLineArr: any[] = [];
+
+          for (const i in resultData) {
+            const geometry = resultData[i].geometry;
+            const properties = resultData[i].properties;
+
+            if (geometry.type === "LineString") {
+              const drawInfoArr: any[] = [];
+
+              for (const j in geometry.coordinates) {
+                const latlng = new window.Tmapv2.Point(
+                  geometry.coordinates[j][0],
+                  geometry.coordinates[j][1]
+                );
+                const convertPoint =
+                  new window.Tmapv2.Projection.convertEPSG3857ToWGS84GEO(
+                    latlng
+                  );
+                const convertChange = new window.Tmapv2.LatLng(
+                  convertPoint._lat,
+                  convertPoint._lng
+                );
+
+                drawInfoArr.push(convertChange);
+              }
+
+              let lineColor = "";
+              const sectionCongestion = properties.congestion;
+              if (sectionCongestion === 0) {
+                lineColor = "#06050D";
+              } else if (sectionCongestion === 1) {
+                lineColor = "#61AB25";
+              } else if (sectionCongestion === 2) {
+                lineColor = "#FFFF00";
+              } else if (sectionCongestion === 3) {
+                lineColor = "#E87506";
+              } else if (sectionCongestion === 4) {
+                lineColor = "#D61125";
+              }
+
+              const polyline = new window.Tmapv2.Polyline({
+                path: drawInfoArr,
+                strokeColor: lineColor,
+                strokeWeight: 6,
+                map: map,
+              });
+              newPolyLineArr.push(polyline);
+            }
+          }
+          setPolyLineArr(newPolyLineArr);
+        })
+        .catch((error) => {
+          console.log("에러:", error);
+        });
     }
   }, [userLocation]);
 
