df848e51a284a33d1bbeac21b979146860f119c9
wjkk21 <verstand821@gmail.com>
2023-06-29 10:48:36 +0900

    gps 버튼 추가

--- Client/view/pages/mainPage/mainPage.tsx
+++ Client/view/pages/mainPage/mainPage.tsx
@@ -4,24 +4,26 @@ import { Link, BrowserRouter as Router } from "react-router-dom";
 import NaverMap from "../../../utils/naverMap";
 import { BiSearchAlt2 } from "react-icons/bi";
 import { IoMdMenu } from "react-icons/io";
+import { MdOutlineGpsFixed } from "react-icons/md";
 
-const searchButtonStyle = {
+const searchButtonStyle: React.CSSProperties = {
   position: "sticky",
   bottom: "30px",
   fontSize: "42px",
   display: "flex",
-  width: "345px",
 };
 
 export default function main() {
   return (
     <>
-      <IoMdMenu fontSize="32px" />
+      <IoMdMenu fontSize="40px" />
       <div style={searchButtonStyle}>
         <Link to="/search">
-          <BiSearchAlt2 color="#000000" fontSize="32px" />
+          <BiSearchAlt2 color="#000000" fontSize="40px" />
         </Link>
       </div>
+      <div className="PlayButton"></div>
+      <MdOutlineGpsFixed fontSize="40px" />
       <NaverMap />
     </>
   );
