f78a86016ae2f4250a1698e41cb5f2519163da27
jiminKang <kang@jiminKangs-MacBook-Pro.local>
2023-06-29 12:48:18 +0900

    htm형식을 리액트로 변환하기 위한 작업. 일단 get요청 데이터가 어떤거 오는 지 확인

--- Client/view/component/maps.jsx
+++ Client/view/component/maps.jsx
@@ -1,45 +1,13 @@
 import React, { useEffect } from 'react';
-// declare global {
-//   interface Window {
-//     kakao: any;
-//   }
-// }
-const Maps = () => {
+import Tmapv2 from 'tmapv2'; // Tmapv2 라이브러리를 import 해주세요.
+
+function MapComponent() {
   useEffect(() => {
-    const container = document.getElementById('map');
-    const script = document.createElement('script');
-    script.async = true;
-    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=b3da2e1025f79e2251178086a01fa93b&autoload=false`;
-    document.head.appendChild(script);
-    script.onload = () => {
-      window.kakao.maps.load(() => {
-        const options = {
-          center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-          level: 3
-        };
-        const map = new window.kakao.maps.Map(container, options);
-        // 사용자의 현재 위치 가져오기
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            const latitude = position.coords.latitude;
-            const longitude = position.coords.longitude;
-            // 현재 위치 마커 생성
-            const markerPosition = new window.kakao.maps.LatLng(latitude, longitude);
-            const marker = new window.kakao.maps.Marker({
-              position: markerPosition
-            });
-            // 마커를 지도에 추가
-            marker.setMap(map);
-            // 지도 중심 위치 변경
-            map.setCenter(markerPosition);
-          },
-          (error) => {
-            console.log('Error getting geolocation:', error);
-          }
-        );
-      });
-    };
+    fetch("https://code.jquery.com/jquery-3.2.1.min.js").then(res=>res).then(data=>console.log('data1',data))
+    fetch("https://apis.openapi.sk.com/tmap/jsv2?version=1&appKey=FB8ThRVZHU4x1zUhC432j3DKfPOdkpmrajEOCYZe").then(res=>res).then(data=>console.log('data2',data))
   }, []);
-  return <div id="map" style={{ width: '100vw', height: '100vh' }} />;
-};
-export default Maps
+
+  return <div id="map_div"></div>;
+}
+
+export default MapComponent;
