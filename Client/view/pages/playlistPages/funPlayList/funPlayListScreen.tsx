5cb5200b10f10a96b21c3b490c504fc69427ef2d
wjkk21 <verstand821@gmail.com>
2023-06-29 09:43:54 +0900

    funplayList 추가

---
+++ Client/view/pages/playlistPages/funPlayList/funPlayListScreen.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import FunPlayList from "./funPlayList";
+
+const Screen: React.FC = () => {
+  return (
+    <>
+      <FunPlayList />
+    </>
+  );
+};
+
+export default Screen;
