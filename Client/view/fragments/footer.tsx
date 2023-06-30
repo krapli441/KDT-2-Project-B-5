2957e459446a8b1cf583aac919a9a76d3cc15cef
wjkk21 <verstand821@gmail.com>
2023-07-01 03:28:03 +0900

    gps 위치가 저기라면 어떨까요

--- Client/view/fragments/footer.tsx
+++ Client/view/fragments/footer.tsx
@@ -1,11 +1,15 @@
+// 리액트 라이브러리
 import React from "react";
 import { Link, BrowserRouter as Router } from "react-router-dom";
+// 리액트 컴포넌트
 import MusicPlayer from "../pages/musicPlayerPage/musicPlayerPageScreen";
 
+// 리액트 아이콘
 import { AiFillHome } from "react-icons/ai";
-import { BiSearchAlt2 } from "react-icons/bi";
 import { IoMdMenu } from "react-icons/io";
+import { BiSearchAlt2 } from "react-icons/bi";
 
+// ? gps 버튼 저기로 바꾸면 어떨까요 귀찮아서 검색이미지로 놔둠
 const navAllStyle: React.CSSProperties = {
   display: "flex",
   justifyContent: "center",
@@ -31,6 +35,7 @@ export default function nav() {
         <div style={navStyle}>
           <IoMdMenu fontSize="60px" color="white" />
           <AiFillHome fontSize="60px" color="white" />
+          <BiSearchAlt2 fontSize="60px" color="white" />
         </div>
       </div>
     </div>
