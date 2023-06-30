f0e6310eab479b2a1d4330d72724ae4a566a1453
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-30 13:00:36 +0900

    app.controller.ts 수정

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -2,17 +2,16 @@ import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Response } from 'express';
 
-@Controller()
+@Controller('*')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  gethtml(@Res()res: Response): void{
-    res.sendFile('index.html',{root: 'public'})
+  gethtml(@Res() res: Response): void {
+    res.sendFile('index.html', { root: 'public' });
   }
   @Get('/bundle.js')
-  getjs(@Res()res: Response): void{
-    res.sendFile('bundle.js',{root: 'public'})
+  getjs(@Res() res: Response): void {
+    res.sendFile('bundle.js', { root: 'public' });
   }
 }
-
