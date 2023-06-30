5c8c5f800a753eac0351a8dd4185d1607ecdcfcc
wjkk21 <verstand821@gmail.com>
2023-06-30 15:33:09 +0900

    overall 플레이 전체 창 생성

---
+++ Client/view/pages/playlistPages/overallPlaylist/overallPlayList.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function overall() {
+  return (
+    <>
+      <div className="overallMain">1</div>
+      <div className="overallMain">2</div>
+      <div className="overallMain">3</div>
+      <div className="overallMain">4</div>
+    </>
+  );
+}
---
+++ Client/view/pages/playlistPages/overallPlaylist/overallPlayListScreen.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import OverallPlayList from "./overallPlayList";
+import "./overall.css";
+
+const Screen: React.FC = () => {
+  return (
+    <>
+      <OverallPlayList />
+    </>
+  );
+};
+
+export default Screen;
