e39f738b1974ecf7638432f37c3324ac9fbca132
박준형 <108733795+krapli441@users.noreply.github.com>
2023-07-01 15:55:49 +0900

    npm에서 google Staatliches 폰트 설치, 회원 가입  페이지에서 추가함. 회원가입 페이지 상단 문구 수정 중.

--- Client/view/pages/signUpPage/signUpSecond.tsx
+++ Client/view/pages/signUpPage/signUpSecond.tsx
@@ -1,15 +1,19 @@
 // 리액트 라이브러리
 import React, { ChangeEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
+
+// 외부 라이브러리
 import {
+  Box,
   FormControl,
   FormLabel,
   FormErrorMessage,
   FormHelperText,
   Input,
   Button,
+  Text,
 } from "@chakra-ui/react";
-
+import "@fontsource/staatliches";
 // 리액트 컴포넌트
 
 const CreateAccount: React.FC = () => {
@@ -22,52 +26,73 @@ const CreateAccount: React.FC = () => {
   const isError = input === "";
 
   return (
-    <FormControl
-      className="createAccountForm"
-      display={"flex"}
-      flexDirection={"column"}
-      width={"80%"}
-      gap={"27px"}
-      isRequired
-    >
-      <Input
-        type="email"
-        placeholder="아이디 (이메일)"
-        height={"60px"}
-        required
-      />
-
-      <Input type="password" placeholder="비밀번호" height={"60px"} required />
-
-      <Input
-        type="password"
-        placeholder="비밀번호 확인"
-        height={"60px"}
-        required
-      />
-      <Input type="text" placeholder="이름" height={"60px"} required />
-      <Input
-        type="number"
-        placeholder="휴대전화번호"
-        height={"60px"}
-        required
-      />
-      <Button
-        mt={4}
-        backgroundColor="#FFB703"
-        color="white"
-        type="submit"
-        width={"340px"}
-        height={"60px"}
-        _hover={{ bg: "#FF8B03" }}
-        _active={{
-          bg: "#FF7C03",
-          transform: "scale(0.98)",
-        }}
+    <>
+      <Box
+        className="header"
+        display={"flex"}
+        justifyContent={"space-between"}
+        width={"80%"}
       >
-        회원가입
-      </Button>
-    </FormControl>
+        <Box className="signUpInfoText">
+          <Text>회원가입</Text>
+          <Text>회원 정보를 입력해주세요.</Text>
+        </Box>
+        <Box className="serviceLogoText">
+          <Text fontFamily={"Staatliches"}>ASPHALT</Text>
+        </Box>
+      </Box>
+      <FormControl
+        className="createAccountForm"
+        display={"flex"}
+        flexDirection={"column"}
+        width={"80%"}
+        gap={"27px"}
+        isRequired
+      >
+        <Input
+          type="email"
+          placeholder="아이디 (이메일)"
+          height={"60px"}
+          required
+        />
+
+        <Input
+          type="password"
+          placeholder="비밀번호"
+          height={"60px"}
+          required
+        />
+
+        <Input
+          type="password"
+          placeholder="비밀번호 확인"
+          height={"60px"}
+          required
+        />
+        <Input type="text" placeholder="이름" height={"60px"} required />
+        <Input
+          type="number"
+          placeholder="휴대전화번호"
+          height={"60px"}
+          required
+        />
+        <Button
+          mt={4}
+          backgroundColor="#FFB703"
+          color="white"
+          type="submit"
+          width={"340px"}
+          height={"60px"}
+          _hover={{ bg: "#FF8B03" }}
+          _active={{
+            bg: "#FF7C03",
+            transform: "scale(0.98)",
+          }}
+        >
+          회원가입
+        </Button>
+      </FormControl>
+    </>
   );
 };
 
