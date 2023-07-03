ba24e7b2cd12954263c1395db5ceb2df91ed0093
박준형 <108733795+krapli441@users.noreply.github.com>
2023-07-03 13:04:47 +0900

    마커 갱신 오류

--- Client/utils/createTmapAPI.tsx
+++ Client/utils/createTmapAPI.tsx
@@ -16,19 +16,18 @@ declare global {
     Tmapv3: any;
   }
 }
-
 const MapContainer: React.FC = () => {
   const [userLocation, setUserLocation] =
     useState<GeolocationCoordinates | null>(null);
+  const [map, setMap] = useState<any>(null);
   const [polyLineArr, setPolyLineArr] = useState<any[]>([]);
-  const [map, setMap] = useState<any[]>([]);
-
-  // * 사용자 위치 정보를 가져오도록 하는 getCurrentPosition 메서드
+  const [marker, setMarker] = useState<any>(null);
   useEffect(() => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
+      const watchId = navigator.geolocation.watchPosition(
         (position) => {
           setUserLocation(position.coords);
+          console.log(position.coords.latitude, position.coords.longitude);
         },
         (error) => {
           console.log(error);
@@ -37,177 +36,131 @@ const MapContainer: React.FC = () => {
           enableHighAccuracy: true,
         }
       );
+      return () => {
+        navigator.geolocation.clearWatch(watchId);
+      };
     } else {
       console.log("사용자 환경이 위치 정보를 제공하지 않습니다.");
     }
   }, []);
-
-  // * SK open API를 사용하여 맵을 생성하는 useEffect 훅
   useEffect(() => {
     if (userLocation) {
       function generateMap() {
-        let map = new window.Tmapv3.Map("tMapContainer", {
-          center: new window.Tmapv3.LatLng(
-            userLocation?.latitude,
-            userLocation?.longitude
-          ),
+        const map = new window.Tmapv3.Map("TmapApp", {
           width: "100%",
-          height: "80%",
+          height: "100%",
           zoom: 15,
         });
-        var marker = new window.Tmapv3.Marker({
+        const marker = new window.Tmapv3.Marker({
           position: new window.Tmapv3.LatLng(
             userLocation?.latitude,
             userLocation?.longitude
           ),
           map: map,
         });
+        setMap(map);
+        setMarker(marker);
         return map;
       }
       const map = generateMap();
-      setMap(map);
-    }
-  }, [userLocation]);
-
-  // * 사용자 위치 정보가 변경될 때마다 호출되는 watchPosition 메서드
-  useEffect(() => {
-    if (navigator.geolocation) {
-      const options = {
-        enableHighAccuracy: true,
-        timeout: 10000,
-      };
-      navigator.geolocation.watchPosition(
-        (position) => {
-          var marker = new window.Tmapv3.Marker({
-            position: new window.Tmapv3.LatLng(
-              userLocation?.latitude,
-              userLocation?.longitude
-            ),
-            map: window.Tmapv3.map,
-          });
-          console.log(
-            "현재 위치:",
-            position.coords.latitude,
-            position.coords.longitude
-          );
-        },
-        (error) => {
-          if (error.code === error.TIMEOUT) {
-            console.log("위치 정보를 가져오는 중 시간이 초과되었습니다.");
-          } else {
-            console.log(
-              "실시간 위치 정보를 불러오는 중 오류가 발생했습니다.",
-              error
-            );
-          }
-        },
-        options
-      );
-    }
-  }, [userLocation]);
-
-  // ! 사용자 위치 정보를 가져오는 useEffect
-  useEffect(() => {
-    if (userLocation) {
-      if (map) {
-        const trafficPointRequestURI = `https://apis.openapi.sk.com/tmap/traffic?version=${PointTrafficSampleData.version}&format=json&reqCoordType=${PointTrafficSampleData.reqCoordType}&resCoordType=${PointTrafficSampleData.resCoordType}&centerLat=${userLocation.latitude}&centerLon=${userLocation.longitude}&trafficType=${PointTrafficSampleData.trafficType}&zoomLevel=${PointTrafficSampleData.zoomLevel}&callback=${PointTrafficSampleData.callback}&appKey=${PointTrafficSampleData.appKey}`;
-
-        fetch(trafficPointRequestURI, {
-          method: "GET",
-        })
-          .then((response) => {
-            if (!response.ok) {
-              throw new Error("현재 위치 교통정보 요청이 실패하였습니다.");
-            }
-            return response.json();
-          })
-          .then((data) => {
-            console.log("현재 위치 교통정보를 성공적으로 수신했습니다.");
-            const resultData = data.features;
-            console.log(resultData);
-          });
-
-        // ! 사용자 주변 교통정보 데이터를 가져오는 fetch
-        const trafficAroundRequestURI = `https://apis.openapi.sk.com/tmap/traffic?version=${AroundTrafficSampleData.version}&format=json&reqCoordType=${AroundTrafficSampleData.reqCoordType}&resCoordType=${AroundTrafficSampleData.resCoordType}&centerLat=${userLocation.latitude}&centerLon=${userLocation.longitude}&trafficType=${AroundTrafficSampleData.trafficType}&radius=${AroundTrafficSampleData.radius}&zoomLevel=${AroundTrafficSampleData.zoomLevel}&callback=${AroundTrafficSampleData.callback}&appKey=${AroundTrafficSampleData.appKey}`;
-
-        fetch(trafficAroundRequestURI, {
-          method: "GET",
-        })
-          .then((response) => {
-            if (!response.ok) {
-              throw new Error("사용자 주변 교통정보 요청이 실패하였습니다.");
-            }
-            return response.json();
-          })
-          .then((data) => {
-            console.log("사용자 주변 교통정보를 성공적으로 수신했습니다.");
-            const resultData = data.features;
-            console.log(resultData);
-
-            if (polyLineArr.length > 0) {
-              polyLineArr.forEach((polyline) => {
-                polyline.setMap(null);
-              });
-            }
-
-            const newPolyLineArr: any[] = [];
-
-            for (const i in resultData) {
-              const geometry = resultData[i].geometry;
-              const properties = resultData[i].properties;
-
-              if (geometry.type === "LineString") {
-                const drawInfoArr: any[] = [];
-
-                for (const j in geometry.coordinates) {
-                  const latlng = new window.Tmapv3.Point(
-                    geometry.coordinates[j][0],
-                    geometry.coordinates[j][1]
-                  );
-                  const convertPoint =
-                    new window.Tmapv3.Projection.convertEPSG3857ToWGS84GEO(
-                      latlng
-                    );
-                  const convertChange = new window.Tmapv3.LatLng(
-                    convertPoint._lat,
-                    convertPoint._lng
-                  );
-
-                  drawInfoArr.push(convertChange);
-                }
-
-                let lineColor = "";
-                const sectionCongestion = properties.congestion;
-                if (sectionCongestion === 0) {
-                  lineColor = "#06050D";
-                } else if (sectionCongestion === 1) {
-                  lineColor = "#61AB25";
-                } else if (sectionCongestion === 2) {
-                  lineColor = "#FFFF00";
-                } else if (sectionCongestion === 3) {
-                  lineColor = "#E87506";
-                } else if (sectionCongestion === 4) {
-                  lineColor = "#D61125";
-                }
-
-                const polyline = new window.Tmapv3.Polyline({
-                  path: drawInfoArr,
-                  strokeColor: lineColor,
-                  strokeWeight: 6,
-                  map: map,
-                });
-                newPolyLineArr.push(polyline);
+      const fetchTrafficData = () => {
+        if (map) {
+          const requestURI = `https://apis.openapi.sk.com/tmap/traffic?version=${AroundTrafficSampleData.version}&format=json&reqCoordType=${AroundTrafficSampleData.reqCoordType}&resCoordType=${AroundTrafficSampleData.resCoordType}&centerLat=${userLocation.latitude}&centerLon=${userLocation.longitude}&trafficType=${AroundTrafficSampleData.trafficType}&zoomLevel=${AroundTrafficSampleData.zoomLevel}&callback=${AroundTrafficSampleData.callback}&appKey=${AroundTrafficSampleData.appKey}`;
+          fetch(requestURI)
+            .then((response) => {
+              if (!response.ok) {
+                throw new Error("요청이 실패하였습니다.");
               }
-            }
-            setPolyLineArr(newPolyLineArr);
-          })
-          .catch((error) => {
-            console.log("에러:", error);
+              return response.json();
+            })
+            .then((data) => {
+              console.log("요청이 성공하였습니다.");
+              const resultData = data.features;
+              const congestionValues = resultData.map(
+                (item: any) => item.properties.congestion
+              );
+              console.log(congestionValues);
+              if (polyLineArr.length > 0) {
+                polyLineArr.forEach((polyline) => {
+                  polyline.setMap(null);
+                });
+              }
+              const newPolyLineArr: any[] = [];
+              for (const i in resultData) {
+                const geometry = resultData[i].geometry;
+                const properties = resultData[i].properties;
+                if (geometry.type === "LineString") {
+                  const drawInfoArr: any[] = [];
+                  for (const j in geometry.coordinates) {
+                    const latlng = new window.Tmapv3.Point(
+                      geometry.coordinates[j][0],
+                      geometry.coordinates[j][1]
+                    );
+                    const convertPoint =
+                      new window.Tmapv3.Projection.convertEPSG3857ToWGS84GEO(
+                        latlng
+                      );
+                    const convertChange = new window.Tmapv3.LatLng(
+                      convertPoint._lat,
+                      convertPoint._lng
+                    );
+                    drawInfoArr.push(convertChange);
+                  }
+                  let lineColor = "";
+                  const sectionCongestion = properties.congestion;
+                  if (sectionCongestion === 0) {
+                    lineColor = "#06050D";
+                  } else if (sectionCongestion === 1) {
+                    lineColor = "#61AB25";
+                  } else if (sectionCongestion === 2) {
+                    lineColor = "#FFFF00";
+                  } else if (sectionCongestion === 3) {
+                    lineColor = "#FF7200";
+                  } else if (sectionCongestion === 4) {
+                    lineColor = "#FF0000";
+                  }
+                  const polyline = new window.Tmapv3.Polyline({
+                    path: drawInfoArr,
+                    strokeColor: lineColor,
+                    strokeWeight: 6,
+                    map: map,
+                  });
+                  newPolyLineArr.push(polyline);
+                }
+              }
+              setPolyLineArr(newPolyLineArr);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+      };
+      const intervalId = setInterval(fetchTrafficData, 5000);
+      return () => {
+        clearInterval(intervalId);
+        if (polyLineArr.length > 0) {
+          polyLineArr.forEach((polyline) => {
+            polyline.setMap(null);
           });
+        }
+        if (marker) {
+          marker.setMap(null);
+        }
+      };
+    }
+  }, [userLocation]);
+  useEffect(() => {
+    if (map && userLocation) {
+      const centerLatLng = new window.Tmapv3.LatLng(
+        userLocation.latitude,
+        userLocation.longitude
+      );
+      map.setCenter(centerLatLng);
+      if (marker) {
+        setMarker(marker.setPosition(centerLatLng));
       }
     }
-  }, [userLocation, map]);
-
+  }, [map, marker, userLocation]);
   return (
     <>
       <Box
