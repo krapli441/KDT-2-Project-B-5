4272733b674d557c5b77b244efe63a97f57ed235
jiminKang <kang@jiminKangs-MacBook-Pro.local>
2023-07-03 22:56:38 +0900

    일단 혼잡도에 따른 유튜브 검색 구현

--- Client/view/pages/youtubeText/youtubeText.tsx
+++ Client/view/pages/youtubeText/youtubeText.tsx
@@ -29,7 +29,7 @@ const YoutubeSearch: React.FC = () => {
     try {
       // 유튜브 API 호출
       const response = await fetch(
-        `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${congestion}&key=AIzaSyA9zqB9QJLqjy1iCTGfgdTNrSo6WpJfRd0`
+        `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${congestion}&key=AIzaSyA9zqB9QJLqjy1iCTGfgdTNrSo6WpJfRd`
       );
 
       if (!response.ok) {
