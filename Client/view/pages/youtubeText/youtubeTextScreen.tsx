ff787086f84f220361c45c68ca48dbd60b74c5df
jiminKang <kang@jiminKangs-MacBook-Pro.local>
2023-07-03 15:46:37 +0900

    react props 문법 사용. 이해 필요

---
+++ Client/view/pages/youtubeText/youtubeTextScreen.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Youtube from './youtubeText'
+
+
+const Screen: React.FC = () => {
+  return (
+    <>
+      <Youtube videoId="bennzino"/>
+    </>
+  );
+};
+
+export default Screen;
