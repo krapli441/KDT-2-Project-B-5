b8edff4916dbdeffca5718d4255af29cf3e87a95
박준형 <108733795+krapli441@users.noreply.github.com>
2023-07-04 10:49:35 +0900

    10초마다 혼잡도를 그리고 있지만 중첩되고 있음

--- Client/utils/createTmapAPI.tsx
+++ Client/utils/createTmapAPI.tsx
@@ -157,10 +157,8 @@ const MapContainer: React.FC = () => {
     }
   }, [userRealTimeLocation]);
 
-  // 10초마다 실행되는 함수
-  const getPointTrafficData = () => {
-    const requestURI = `https://apis.openapi.sk.com/tmap/traffic?version=${TrafficPointData.version}&format=json&reqCoordType=${TrafficPointData.reqCoordType}&resCoordType=${TrafficPointData.resCoordType}&centerLat=${userRealTimeLocation?.latitude}&centerLon=${userRealTimeLocation?.longitude}&trafficType=${TrafficPointData.trafficType}&zoomLevel=${TrafficPointData.zoomLevel}&callback=${TrafficPointData.callback}&appKey=${TrafficPointData.appKey}`;
-
+  const getAroundTrafficData = () => {
+    const requestURI = `https://apis.openapi.sk.com/tmap/traffic?version=${TrafficAroundData.version}&format=json&reqCoordType=${TrafficAroundData.reqCoordType}&resCoordType=${TrafficAroundData.resCoordType}&centerLat=${userRealTimeLocation?.latitude}&centerLon=${userRealTimeLocation?.longitude}&trafficType=${TrafficAroundData.trafficType}&zoomLevel=${TrafficAroundData.zoomLevel}&callback=${TrafficAroundData.callback}&appKey=${TrafficAroundData.appKey}`;
     fetch(requestURI)
       .then((response) => {
         if (!response.ok) {
@@ -176,7 +174,6 @@ const MapContainer: React.FC = () => {
         );
         //! 사용자 위치의 도로 교통 정보(혼잡도)를 나타내는 부분
         console.log(congestionValues); // ['2'], length: 1, Prototype: Array(0)
-
         if (polyLineArr.length > 0) {
           polyLineArr.forEach((polyline) => {
             polyline.setMap(null);
@@ -237,15 +234,42 @@ const MapContainer: React.FC = () => {
       });
   };
 
+  // 10초마다 실행되는 함수
+  const getPointTrafficData = () => {
+    const requestURI = `https://apis.openapi.sk.com/tmap/traffic?version=${TrafficPointData.version}&format=json&reqCoordType=${TrafficPointData.reqCoordType}&resCoordType=${TrafficPointData.resCoordType}&centerLat=${userRealTimeLocation?.latitude}&centerLon=${userRealTimeLocation?.longitude}&trafficType=${TrafficPointData.trafficType}&zoomLevel=${TrafficPointData.zoomLevel}&callback=${TrafficPointData.callback}&appKey=${TrafficPointData.appKey}`;
+
+    fetch(requestURI)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("요청이 실패하였습니다.");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("요청이 성공하였습니다.");
+        const resultData = data.features;
+        const congestionValues = resultData.map(
+          (item: any) => item.properties.congestion
+        );
+        //! 사용자 위치의 도로 교통 정보(혼잡도)를 나타내는 부분
+        console.log(congestionValues); // ['2'], length: 1, Prototype: Array(0)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   // 5초 뒤에 함수 실행
   useEffect(() => {
     const timeoutId = setTimeout(() => {
-      getPointTrafficData(); // 함수 실행
-      const intervalId = setInterval(getPointTrafficData, 10000); // 10초마다 반복 실행
-
+      getPointTrafficData(); // 사용자 위치 교통정보 요청 함수 실행
+      getAroundTrafficData(); // 사용자 주변 교통정보 요청 함수 실행
+      const intervalPoint = setInterval(getPointTrafficData, 10000); // 10초마다 반복 실행
+      const intervalAround = setInterval(getAroundTrafficData, 10000); // 10초마다 반복 실행
       // 컴포넌트가 언마운트될 때 interval 제거
       return () => {
-        clearInterval(intervalId);
+        clearInterval(intervalPoint);
+        clearInterval(intervalAround);
       };
     }, 5000);
 
