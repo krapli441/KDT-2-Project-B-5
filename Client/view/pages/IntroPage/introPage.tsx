3200d057a5b739941583eb8419b40b033e0561c8
박준형 <108733795+krapli441@users.noreply.github.com>
2023-07-04 20:14:54 +0900

    교통 정보에 따른 위치 표시 수정

--- Client/view/pages/IntroPage/introPage.tsx
+++ Client/view/pages/IntroPage/introPage.tsx
@@ -3,12 +3,10 @@ import React, { useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { Box } from "@chakra-ui/react";
 import "@dotlottie/player-component";
-import { AuthContext } from "../../../utils/trafficCongestionContext";
 // 리액트 컴포넌트
 import IntroLogo from "./introLogo.png";
 
 function IntroPage() {
-  const { setCongestion } = useContext(AuthContext);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -31,7 +29,6 @@ function IntroPage() {
           autoplay
           style={{ width: "100%", height: "100%" }}
         />
-        {/* <img src={IntroLogo} width="70%" alt="로고" /> */}
       </Box>
     </>
   );
