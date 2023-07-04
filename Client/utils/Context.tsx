2ac3d4353d381c229e3e34f6d6c918af3ef0ebbf
jiminKang <wlals1234560@gmail.com>
2023-07-04 16:41:11 +0900

    준형님 코드 pull 전 커밋

--- Client/utils/Context.tsx
+++ Client/utils/Context.tsx
@@ -3,21 +3,29 @@ import React, { useState, createContext } from "react";
 interface AuthContextProps {
     congestion:string;
     setCongestion:(congestion:string) => void;
+    isPlaying:boolean,
+    setIsPlaying:(isPlaying:boolean) =>void;
 
 }
 
 export const AuthContext = createContext<AuthContextProps>({
     congestion:'',
-    setCongestion:() => {}
+    setCongestion:() => {},
+    isPlaying:false,
+    setIsPlaying:()=> {}
 });
 
 export const AuthProvider:React.FC<{children:React.ReactNode}> = ({children})=>{
     const [congestion, setCongestion] = useState('빈지노');
+    const [isPlaying, setIsPlaying] = useState(false)
 
     return (
         <AuthContext.Provider value={{
             congestion,
-            setCongestion,   
+            setCongestion,
+            isPlaying,
+            setIsPlaying
+
         }}>
              {children}
 
