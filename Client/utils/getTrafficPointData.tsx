075651d3f7957958bf4d5fc36fb2112c5e7d635f
박준형 <108733795+krapli441@users.noreply.github.com>
2023-07-06 14:24:41 +0900

    API키 변경

--- Client/utils/getTrafficPointData.tsx
+++ Client/utils/getTrafficPointData.tsx
@@ -65,7 +65,7 @@ const SampleData = {
   callback: "function",
 
   // ? appKey : 발급받은 APP Key
-  appKey: "4AfhmXH1W616IshxuKXD27orRe3ufLzD4EHChwyV",
+  appKey: "bEukn9QeYC1tnx9OAck4i9m5fTS6DscR44zbAfSY",
 };
 
 export default SampleData;
