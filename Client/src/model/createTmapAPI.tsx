cf075c5c0aac73d02d3d07b3ffd7984d72e57268
박준형 <108733795+krapli441@users.noreply.github.com>
2023-07-11 12:26:04 +0900

    교통정보 요청 함수를 분리시키기

--- Client/src/model/createTmapAPI.tsx
+++ Client/src/model/createTmapAPI.tsx
@@ -231,33 +231,31 @@ const MapContainer: React.FC = () => {
 
   return (
     <>
-      <>
-        {!isMapReady && <Header />}
-        <Box id="tMapContainer" position={"sticky"}>
-          {isMapReady && (
-            <dotlottie-player
-              src="https://lottie.host/21edb5c7-0e2f-41fe-bd21-12a0c246b066/0ajUoSiKvd.json"
-              autoplay
-              loop
-              style={{ width: "100%", height: "100%" }}
-            />
-          )}
-        </Box>
-        {!isMapReady && (
-          <Box
-            className="navigationBar"
-            display={"flex"}
-            flexDirection={"column"}
-            width={"100%"}
-            height={"20%"}
-            fontSize={"24px"}
-            backgroundColor={"#21325E"}
-            borderRadius={"10% 10% 0% 0%;"}
-          >
-            <YoutubePlayer />
-          </Box>
+      {!isMapReady && <Header />}
+      <Box id="tMapContainer" position={"sticky"}>
+        {isMapReady && (
+          <dotlottie-player
+            src="https://lottie.host/21edb5c7-0e2f-41fe-bd21-12a0c246b066/0ajUoSiKvd.json"
+            autoplay
+            loop
+            style={{ width: "100%", height: "100%" }}
+          />
         )}
-      </>
+      </Box>
+      {!isMapReady && (
+        <Box
+          className="navigationBar"
+          display={"flex"}
+          flexDirection={"column"}
+          width={"100%"}
+          height={"20%"}
+          fontSize={"24px"}
+          backgroundColor={"#21325E"}
+          borderRadius={"10% 10% 0% 0%;"}
+        >
+          <YoutubePlayer />
+        </Box>
+      )}
     </>
   );
 };
---
+++ Client/src/model/trafficRequestURI.ts
@@ -0,0 +1,7 @@
+import TrafficPointData from "./getTrafficPointData"
+
+
+
+function reqeustTrafficData () {
+
+}
