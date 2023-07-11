705194725486bcf6b386afd80618488445b2b359
박준형 <108733795+krapli441@users.noreply.github.com>
2023-07-11 10:28:28 +0900

    파일명 변경 및 주석 추가

--- Client/utils/getDistance.ts
+++ Client/utils/getDistance.ts
@@ -1,18 +1,20 @@
+// 현재 위치와 이전 위치 거리를 측정하는 함수
+
 function getDistance(
   coord1: GeolocationCoordinates,
   coord2: GeolocationCoordinates
 ) {
-  const earthRadius = 6371; // 지구 반지름 (단위: km)
-  const latDiff = toRadians(coord2.latitude - coord1.latitude);
-  const lngDiff = toRadians(coord2.longitude - coord1.longitude);
-  const a =
+  const earthRadius: number = 6371; // 지구 반지름 (단위: km)
+  const latDiff: number = toRadians(coord2.latitude - coord1.latitude);
+  const lngDiff: number = toRadians(coord2.longitude - coord1.longitude);
+  const a: number =
     Math.sin(latDiff / 2) * Math.sin(latDiff / 2) +
     Math.cos(toRadians(coord1.latitude)) *
       Math.cos(toRadians(coord2.latitude)) *
       Math.sin(lngDiff / 2) *
       Math.sin(lngDiff / 2);
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  const distance = earthRadius * c;
+  const c: number = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const distance: number = earthRadius * c;
   return distance;
 }
 
