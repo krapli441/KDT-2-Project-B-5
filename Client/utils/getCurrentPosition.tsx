2af678d73b5dbdf3283d56b0ded5d0c5ead2375a
jiminKang <wlals1234560@gmail.com>
2023-07-13 16:49:08 +0900

    useContext는 react컴포넌트 내에서만 호출 해야함

--- Client/utils/getCurrentPosition.tsx
+++ Client/utils/getCurrentPosition.tsx
@@ -1,10 +1,7 @@
-import React, { useEffect, useState,useContext } from "react";
-import { AuthContext } from "./trafficCongestionContext";
 
 
 
 function getCurrentPosition():Promise<GeolocationCoordinates> {
-    const {userCurrentLocation,setUserCurrentLocation}= useContext(AuthContext)     
       return new Promise((resolve,reject)=>{
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
