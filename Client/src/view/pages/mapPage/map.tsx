fbc1e430cc11ab6f4aa93ebf1496cb5a1f6b9569
jiminKang <wlals1234560@gmail.com>
2023-07-14 12:41:16 +0900

    병합 완료. 메인 페이지는 createTmapAPIVer2(유튜브 재생이 끝날 때 마다 교통 정보 요청)

--- Client/src/view/pages/mapPage/map.tsx
+++ Client/src/view/pages/mapPage/map.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Tmap from "../../../model/createTmapAPI";
+import Tmap from "../../../model/createTmapAPIVer2";
 
 const Map: React.FC = () => {
   return (
---
+++ Client/src/view/pages/youtubePlayer/youtubePlayerVer1.tsx
@@ -0,0 +1,114 @@
+import React, { useContext, useState, useEffect } from "react";
+import YouTube from "react-youtube";
+import { AuthContext } from "../../../model/trafficCongestionContext";
+
+const VideoPlayer = () => {
+  const trafficStatusZero = [
+    "aUndbCBVV0c",
+    "btPJPFnesV4",
+    "mNEUkkoUoIA",
+    "vx2u5uUu3DE",
+    "9jK-NcRmVcw",
+    "IcrbM1l_BoI",
+  ];
+  const trafficStatusOne = [
+    "nERQ6u1NZgA",
+    "ApXoWvfEYVU",
+    "r1hjXK1kzOc",
+    "7Arb-XUpSRk",
+    "2Y6Nne8RvaA",
+    "iArXv64tCJA",
+  ];
+  const trafficStatusTwo = [
+    "s2qAzRWg5VQ",
+    "uu_3AU3-a9E",
+    "AAOyOZ3GeZ0",
+    "ubZrAmRxy_M",
+    "CfbrBV5jXO0",
+    "dkdoawhm0i4",
+  ];
+  const trafficStatusThree = [
+    "CP9PXe74mW8",
+    "00LLwjonJv4",
+    "w4qYzE9hTto",
+    "vmDDOFXSgAs",
+    "2XBrycTHq4Y",
+    "d0Uj0K9mcoQ",
+  ];
+
+  const trafficStatusFour = ["4Ukh9aQBzWc", "hAjiKVEWZSk", "kON9fn01rUQ"];
+  const openingVideo = "DLr8m_W3HbU";
+
+  const opts = {
+    height: "100%",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const [currentVideoId, setCurrentVideoId] = useState(openingVideo);
+  const { congestion, isPlaying } = useContext(AuthContext);
+
+  const onReady = (event: any) => {
+    event.target.playVideo();
+  };
+
+  const onPlayerStateChange = (event: any) => {
+    if (event.data === 0) {
+      console.log("test=", congestion);
+
+      // 랜덤 Video ID 선택
+      let randomVideoId = currentVideoId;
+
+      // 현재 Video ID와 다른 값이 나올 때까지 반복해서 선택
+      while (randomVideoId === currentVideoId) {
+        const randomIndex = Math.floor(Math.random() * 3);
+        console.log("randomIndex:", randomIndex);
+
+        if (congestion === "정보 없음") {
+          randomVideoId = trafficStatusZero[randomIndex];
+        } else if (congestion === "원활") {
+          randomVideoId = trafficStatusOne[randomIndex];
+        } else if (congestion === "서행") {
+          randomVideoId = trafficStatusTwo[randomIndex];
+        } else if (congestion === "지체") {
+          randomVideoId = trafficStatusThree[randomIndex];
+        } else if (congestion === "정체") {
+          randomVideoId = trafficStatusFour[randomIndex];
+        }
+      }
+
+      setCurrentVideoId(randomVideoId);
+    }
+  };
+
+  useEffect(() => {
+    const player = document.getElementById("youtube-player");
+    if (player) {
+      player.addEventListener("onStateChange", onPlayerStateChange);
+    }
+    return () => {
+      if (player) {
+        player.removeEventListener("onStateChange", onPlayerStateChange);
+      }
+    };
+  }, [currentVideoId]);
+
+  return (
+    <>
+      <YouTube
+        id="youtube-player"
+        className="player"
+        style={{ width: "100%", height: "100%" }}
+        videoId={currentVideoId}
+        opts={opts}
+        onReady={onReady}
+        key={currentVideoId}
+        onStateChange={onPlayerStateChange}
+      />
+    </>
+  );
+};
+
+export default VideoPlayer;
